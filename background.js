9936004178302d5159c2d86cda38a0b9b536cde5
FUKUMOTO Yuki <rk76fewf@gmail.com>
2023-02-12 23:42:36 +0900

    [Add] 雛形をちょっと変更しただけ

---
+++ background.js
@@ -0,0 +1,15 @@
+var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41';
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'User-Agent') {
+                details.requestHeaders[i].value = ua;
+                break;
+            }
+        }
+        return {requestHeaders: details.requestHeaders};
+    },
+    {urls: ["*://*.bing.com/*"]},
+    ["blocking", "requestHeaders"]
+);
